9808acba1f89c80c09bbd75d45a1ba4cd087fe6b
Rizwan Shaikh <135222512+Web-Rix@users.noreply.github.com>
2023-11-19 13:14:24 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route  } from 'react-router-dom'
+import Home from './components/Home'
+import SingleMovie from './components/SingleMovie'
+import Error from './components/Error'
 
 const App = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='movie/:id' element={<SingleMovie />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+    </>
   )
 }
 
--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const SingleMovie = () => {
+  const { id } = useParams()
+  
   return (
     <div>
-      our SingleMovie page
+      our SingleMovie page 
     </div>
   )
 }
