cc573a697c2716241daea66da6c3a1a580c53337
Rizwan Shaikh <135222512+Web-Rix@users.noreply.github.com>
2023-11-19 19:19:09 +0530

    context created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useGlobalContext } from '../context'
 
 const Home = () => {
+  const userName = useGlobalContext();
   return (
-    <div>
-      home
-    </div>
+    <>
+      
+    </>
   )
 }
 
