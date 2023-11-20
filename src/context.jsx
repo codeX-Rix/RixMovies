34281e0e3e15d460b69bed95df9cf4a3b88f30af
Rizwan Shaikh <135222512+Web-Rix@users.noreply.github.com>
2023-11-20 11:10:00 +0530

    imdb api setup added

--- src/context.jsx
+++ src/context.jsx
@@ -1,13 +1,43 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext();
 
+const API_URL = "http://www.omdbapi.com/?apikey=76f24ca8&s=batman";
+
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={"rixShaikh"}>{children}</AppContext.Provider>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const [isError, setIsError] = useState({ show: false, msg: "" });
+  const getMovies = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      if (data.Response === "True") {
+        setIsLoading(false);
+        setMovie(data.Search);
+      } else {
+        setIsError({
+          show: true,
+          msg: data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(API_URL);
+  });
+
+  return (
+    <AppContext.Provider value={"rixShaikh"}>{children}</AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider, useGlobalContext};
+export { AppContext, AppProvider, useGlobalContext };
