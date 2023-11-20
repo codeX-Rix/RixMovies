abf3b6881b1a11f3b714dd05b9433a61ec7704bf
Rizwan Shaikh <135222512+Web-Rix@users.noreply.github.com>
2023-11-20 11:10:00 +0530

    imdb api setup added

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,37 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext();
 
+const API_URL = 'http://www.omdbapi.com/?apikey=76f24ca8&s=batman'
+
 const AppProvider = ({ children }) => {
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [movie, setMovie] = useState([]);
+  const [isError, setIsError] = useState({ show: false, msg: "" })
+  const getMovies = async (url) => {
+    try{
+      const res = await fetch(url);
+      const data = await res.json();
+      
+      if(data.Response === "True"){
+        
+        setMovie(data.Search)
+      }else{
+        setIsError({
+          show: true,
+          msg: data.error
+        })
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies(API_URL)
+  })
+
   return <AppContext.Provider value={"rixShaikh"}>{children}</AppContext.Provider>;
 };
 
